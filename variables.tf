eff34b0cf743692edf6edc03e2270ae9d99d4a58
yadavprakash <yadavprakash.it@gmail.com>
2024-07-18 11:14:03 +0530

    update module

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "label_order" {
 variable "managedby" {
   type        = string
   default     = ""
-  description = "ManagedBy, eg 'opsstation'."
+  description = "ManagedBy, eg 'yadavprakash'."
 }
 
 variable "resource_group_name" {
